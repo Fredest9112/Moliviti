09eca57d3f77838ee9357ca88be412575d110e43
fredest <fredest9112@gmail.com>
2022-11-20 15:28:49 -0500

    setting navdrawer

--- app/src/main/java/com/example/android/moliviti/MainActivity.kt
+++ app/src/main/java/com/example/android/moliviti/MainActivity.kt
@@ -1,11 +1,69 @@
 package com.example.android.moliviti
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.android.moliviti.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var navController: NavController
+    private lateinit var navHostFragment: NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        toggle = ActionBarDrawerToggle(this, binding.drawerLayout, R.string.open, R.string.close)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.navView.setNavigationItemSelectedListener{ menuItem ->
+            when(menuItem.itemId){
+                R.id.nav_home -> {
+                    Toast.makeText(this, "Inicio fue clickeado", Toast.LENGTH_SHORT).show()
+                    binding.drawerLayout.close()
+                }
+                R.id.nav_bus -> {
+                    Toast.makeText(this, "Rutas fue clickeado", Toast.LENGTH_SHORT).show()
+                    binding.drawerLayout.close()
+                }
+                R.id.nav_card -> {
+                    Toast.makeText(this, "Tu llave fue clickeado", Toast.LENGTH_SHORT).show()
+                    binding.drawerLayout.close()
+                }
+                R.id.nav_fav -> {
+                    Toast.makeText(this, "Favoritos fue clickeado", Toast.LENGTH_SHORT).show()
+                    binding.drawerLayout.close()
+                }
+                R.id.nav_about -> {
+                    Toast.makeText(this, "Acerca de fue clickeado", Toast.LENGTH_SHORT).show()
+                    binding.drawerLayout.close()
+                }
+                R.id.nav_settings -> {
+                    Toast.makeText(this, "Configuracion fue clickeado", Toast.LENGTH_SHORT).show()
+                    binding.drawerLayout.close()
+                }
+            }
+            true
+        }
+
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    if (toggle.onOptionsItemSelected(item)){
+        return true
+    }
+        return super.onOptionsItemSelected(item)
     }
 }
